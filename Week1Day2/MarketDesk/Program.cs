516f8b91ee9240b44b34890aac6eb6f98496db3b
DESKTOP-8N8A5Q0\Quyen DotNet <thokimquangquyen@gmail.com>
2020-08-05 16:31:45 +0700

    Refactor "Main" function

--- Week1Day2/MarketDesk/Program.cs
+++ Week1Day2/MarketDesk/Program.cs
@@ -18,47 +18,76 @@ namespace MarketDesk
             {
                 string[] input = Console.ReadLine().Split(',');
 
-                if (input.First() == "x")
+                if (InputExitCharacter(input))
                 {
                     break;
                 }
 
-                if (string.IsNullOrEmpty(input.First()))
+                if (InputEmpty(input))
                 {
                     if (order.IsEmpty())
                     {
-                        Console.WriteLine("Not have value to Order");
+                        Console.WriteLine("Not have value to check Order");
                         continue;
                     }
 
-                    double total = order.CalculateTotalPrice();
-                    double totalWithTax = order.CalculateTotalWithTax();
-
+                    double total;
+                    double totalWithTax;
+                    CalculateTotal(order, out total, out totalWithTax);
                     DisplayResult(total, totalWithTax);
                 }
                 else
                 {
-                    try
-                    {
-                        string name = input[0].Trim();
-                        int quantity = Convert.ToInt32(input[1].Trim());
-                        double price = Convert.ToDouble(input[2].Trim());
-                        if (price < 0 || quantity < 0)
-                        {
-                            Console.WriteLine("Price and Quantity cannot be negative");
-                            break;
-                        }
-
-                        order.Items.Add(new OrderItem { Name = name, Price = price, Quantity = quantity });
-                }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("input must contain quantity and price after comma");
-                        continue;
-                    }
+                    GetOrderItemFromInput(input, order);
                 }
             }
             while (true);
+        }
+
+        private static bool InputEmpty(string[] input)
+        {
+            return string.IsNullOrEmpty(input.First());
+        }
+
+        private static bool InputExitCharacter(string[] input)
+        {
+            return input.First() == "x";
+        }
+
+        private static void GetOrderItemFromInput(string[] input, Order order)
+        {
+            try
+            {
+                string name = input[0].Trim();
+                int quantity = Convert.ToInt32(input[1].Trim());
+                double price = Convert.ToDouble(input[2].Trim());
+                if (IsNegative(quantity, price))
+                {
+                    Console.WriteLine("Price and Quantity cannot be negative");
+                }
+
+                AddNewOrderItem(order, name, quantity, price);
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine("Price and Quantity must be decimal");
+            }
+        }
+
+        private static void AddNewOrderItem(Order order, string name, int quantity, double price)
+        {
+            order.Items.Add(new OrderItem { Name = name, Price = price, Quantity = quantity });
+        }
+
+        private static bool IsNegative(int quantity, double price)
+        {
+            return price < 0 || quantity < 0;
+        }
+
+        private static void CalculateTotal(Order order, out double total, out double totalWithTax)
+        {
+            total = order.CalculateTotalPrice();
+            totalWithTax = order.CalculateTotalWithTax();
         }
 
         private static void DisplayResult(double total, double totalWithTax)

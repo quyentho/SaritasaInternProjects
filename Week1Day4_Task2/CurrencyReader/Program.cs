404c5fa61dc53b31b4babfde25efe05480c5187b
Quyen Tho <thokimquangquyen@gmail.com>
2020-08-10 09:28:56 +0700

    Init commit

---
+++ Week1Day4_Task2/CurrencyReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrencyReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

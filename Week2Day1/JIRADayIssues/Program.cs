88a66acbc9cb06a94e07edf69f4287a1207982dc
Quyen Tho <thokimquangquyen@gmail.com>
2020-08-11 17:46:37 +0700

    Add ruleset.

---
+++ Week2Day1/JIRADayIssues/Program.cs
@@ -0,0 +1,14 @@
+﻿using JIRADayIssues.Model;
+using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JIRADayIssues
+{
+    [Command(Name = "Saritasa.JiraChecker", Description = "Check user's hours for the day.")]
+    [HelpOption("--help")]
+    public class Program
+    {
+        public static int Main(string[] args) => CommandLineApplication.Execute<CommandLineOption>(args);
+    }
+}

47e6d0173e11b984c2780fd54559f1b78b92ff0a
Quyen Tho <thokimquangquyen@gmail.com>
2020-08-11 17:33:09 +0700

    Sample using CommandLindUtils

---
+++ Week2Day1/JIRADayIssues.Model/Credential.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JIRADayIssues.Model
+{
+    /// <summary>
+    /// Credentials for authentication.
+    /// </summary>
+    public class Credential
+    {
+        /// <summary>
+        /// Gets or sets username for authentication.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets access token.
+        /// </summary>
+        public string AcessToken { get; set; }
+    }
+}
---
+++ Week2Day1/JIRADayIssues.Service/CommandLineOption.cs
@@ -0,0 +1,39 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JIRADayIssues.Model
+{
+    /// <summary>
+    /// Options for command line .
+    /// </summary>
+    public class CommandLineOption
+    {
+        /// <summary>
+        /// Date to check, if no value, use current date by default.
+        /// </summary>
+        [Option("-d|--date", CommandOptionType.SingleOrNoValue, Description = "Date to check, current date by default")]
+        public (bool hasValue, DateTime value) Date { get; set; }
+
+        /// <summary>
+        /// Username option, required.
+        /// </summary>
+        [Option("-u|--username", CommandOptionType.SingleValue, Description = "Username to check")]
+        [Required]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Token option, required.
+        /// </summary>
+        [Option("-t|--token", CommandOptionType.SingleValue, Description = "Provided token")]
+        [Required]
+        public string Token { get; set; }
+
+        public void OnExecute()
+        {
+            Console.WriteLine($"Date {this.Date.value.ToString()}!");
+            Console.WriteLine($"Hello {this.Username}!");
+            Console.WriteLine($"Token {this.Token}!");
+        }
+    }
+}

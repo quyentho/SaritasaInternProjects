6f3c342feae16d63950b2bcb37bae2dd5c155e86
Quyen Tho <thokimquangquyen@gmail.com>
2020-08-13 11:44:19 +0700

    resolve conflict.

--- Week2Day1/JIRADayIssues/Program.cs
+++ Week2Day1/JIRADayIssues/Program.cs
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-﻿using System;
-
-namespace JIRADayIssues
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-=======
-﻿using JiraDayIssues.Service;
+using JiraDayIssues.Service;
 using McMaster.Extensions.CommandLineUtils;
-
+﻿
 namespace JiraDayIssues
 {
     [Command(Name = "Saritasa.JiraChecker", Description = "Check user's hours for the day.")]
@@ -20,6 +8,5 @@ namespace JiraDayIssues
     public class Program
     {
         public static int Main(string[] args) => CommandLineApplication.Execute<CommandLine>(args);
->>>>>>> feature/Week2Day1
     }
 }

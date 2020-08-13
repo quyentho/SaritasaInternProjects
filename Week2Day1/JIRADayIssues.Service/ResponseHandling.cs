ec08748d8034f3b27f27180b9ce98a4bbe63be6b
Quyen Tho <thokimquangquyen@gmail.com>
2020-08-13 14:44:38 +0700

    Fix display format.

--- Week2Day1/JIRADayIssues.Service/ResponseHandling.cs
+++ Week2Day1/JIRADayIssues.Service/ResponseHandling.cs
@@ -38,9 +38,9 @@ namespace JiraDayIssues.Service
                 var timeSpent = TimeSpan.FromSeconds(issue.Field.TimeSpent);
                 var estimateTime = TimeSpan.FromSeconds(issue.Field.EstimateTime);
                 Console.WriteLine(string.Format("{0,40} {1:0h}/{2:0h}", issue.Field.Summary, timeSpent.TotalHours, estimateTime.TotalHours));
+                Console.WriteLine("\n");
             }
-
-            if(responseObject.Issues.Count > 0)
+            if (responseObject.Issues.Count > 0)
             {
                 Console.WriteLine(string.Format("Total {0:0h} {1:0m}", totalTime.Hours, totalTime.Minutes));
             }

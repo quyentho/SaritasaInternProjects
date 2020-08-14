5f3b8d531f1765c3c1c0642b4d920c91ddc12ade
Quyen Tho <thokimquangquyen@gmail.com>
2020-08-14 13:26:18 +0700

    Finish refactor.

--- Week1Day4_Task2/CurrencyReader.Service/FindCurrencyServiceDecorator.cs
+++ Week1Day4_Task2/CurrencyReader.Service/FindCurrencyServiceDecorator.cs
@@ -21,9 +21,24 @@ namespace CurrencyReader.Service
             this.findCurrencyService = findCurrencyService;
         }
 
+        /// <summary>
+        /// Call to base class and decorate more service.
+        /// </summary>
+        /// <param name="currencies">List currency read from file.</param>
+        /// <param name="input">User input.</param>
+        /// <returns>Currency object if found.</returns>
         public SearchResult GetCurrencies(List<Currency> currencies, string input)
         {
-            throw new NotImplementedException();
+            var result = this.findCurrencyService.GetCurrencies(currencies, input);
+
+            Decorate(result);
+
+            return result;
         }
+
+        /// <summary>
+        /// Additional functionality after find currency.
+        /// </summary>
+        public abstract void Decorate(SearchResult result);
     }
 }

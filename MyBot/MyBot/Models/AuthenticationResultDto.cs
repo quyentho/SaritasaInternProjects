5205dc75bbde1155659ddfb0a645ae2fd9ebc82b
Quyen Tho <thokimquangquyen@gmail.com>
2020-10-26 12:02:23 +0700

    Init commit

---
+++ MyBot/MyBot/Models/AuthenticationResultDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MyBot.Models
+{
+    public class AuthenticationResultDto
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Expires in seconds.
+        /// </summary>
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        public string Scope { get; set; }
+
+        public string Error { get; set; }
+
+        [JsonProperty("require_ssl")]
+        public bool RequireSsl { get; set; }
+    }
+}
